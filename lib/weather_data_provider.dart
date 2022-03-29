ba791f4612532083f7bf50f6f3cb3a1145d3bc8c
nora-strasberg <nora.strasberg@millicom.com>
2022-03-29 15:40:10 -0300

    remove api url and key

--- lib/weather_data_provider.dart
+++ lib/weather_data_provider.dart
@@ -1,12 +1,12 @@
 import 'package:http/http.dart' as http;
 
 class WeatherDataProvider {
-  final String API_KEY = '9f720cdaaad301a44b0722f1b5d3dc80';
+  final String API_KEY = ''; //get api key
+  final String URL_API = ''; // replace with URL from article (readme.md)
 
   Future<http.Response> getRawWeatherData(String city) async {
     http.Response rawWeatherData = await http.get(
-      Uri.parse(
-          'https://api.openweathermap.org/data/2.5/weather?q=$city&appid=$API_KEY&units=metric'),
+      Uri.parse(URL_API),
     );
     return rawWeatherData;
   }
